44b2c0fd11dad2551840385e4a7ace6359383453
Maxime Collet <maxcollet01@gmail.com>
2025-05-31 10:22:09 +0200

    moving to call api

--- public/script.js
+++ public/script.js
@@ -123,7 +123,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // So once it is build as we want it (see HTML for reference), we simply remove the hard coded HTML as we have now a dynamic way to create our cards !
     function createCard(podcast){
         const card = document.createElement("div");
-        card.className = "card";
+        card.className = "card pointer";
 
         const img = document.createElement("img");
         img.src = podcast.image || "./default-podcast.png";
@@ -153,11 +153,39 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         card.appendChild(img);
         card.appendChild(content);
+
+        card.addEventListener("click", () => loadEpisodes(podcast.itunesId, podcast.episodeCount))
         return card
     }
 
 
+// Load episodes
+    async  function loadEpisodes(feedId, count){
+        if (!feedId) return;
 
+        showLoader();
+
+        try {
+            const response = await fetch(`/api/episodes?feedId=${encodeURIComponent(feedId)}&max=${count}`);
+            const data = await response.json();
+            responseContainer.textContent = "";
+            if (data.items && data.items.length > 0){
+                console.log("Episodes:", data.items);
+                // data.feeds.forEach(podcast => {
+                //     const card = createCard(podcast);
+                //     responseContainer.appendChild(card);
+                // })
+            }else {
+                responseContainer.innerText = "No results found";
+            }
+
+        }catch (error){
+            responseContainer.innerText = `Error: ${error.message}`;
+        }
+
+        hideLoader();
+
+    }
 
     // Navigation ------
     const searchLink = document.getElementById("searchLink");
--- server.js
+++ server.js
@@ -61,6 +61,37 @@ app.get("/api/search", async (req,res)=>{
 });
 
 
+// Fetch episodes by Itunes ID
+app.get("/api/episodes", async (req,res)=>{
+    const feedId = req.query.feedId;
+    const max = req.query.max;
+    if (!feedId){
+        return res.status(400).json({error: "Feed ID Parameter is required"});
+    }
+
+    const headers = generateAuthHeader();
+
+    try{
+        const response = await fetch(`${apiEndpoint}/episodes/byitunesid?id=${encodeURIComponent(feedId)}&max=${max}`, {
+            method:"GET",
+            headers: headers
+        });
+
+        if (response.ok && response.headers.get("content-type").includes("application/json")){
+            const data = await  response.json();
+            res.json(data);
+        }else {
+            const rawText = await response.text();
+            console.log("Raw Response:", rawText);
+            res.status(500).json({error:"Invalid response from API", rawText})
+        }
+    }catch (error){
+        console.error("Error fetching API:", error.message);
+        res.status(500).json({error:error.message});
+    }
+});
+
+
 app.listen(PORT, ()=>{
     console.log(`Server is running at port:  http://localhost:${PORT} pointing to ${apiEndpoint}`)
 })
